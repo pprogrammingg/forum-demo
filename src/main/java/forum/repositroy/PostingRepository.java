d112e53514631ac91e81dbff2af5dcc179dcb771
pprog ramming <pprogrammingg@gmail.com>
2019-12-09 03:50:44 -0500

    Handle comments for postings
    A comment is essentially a Posting entity with an extra field, here
    defined as OriginalPostingId, to hold the Id of the Posting it is
    a comment for.
    
    Add repository methods to find comments for a Posting Id and
    modified the getAll controller to only return top level
    postings for cases where findAll() was previously invoked.
    
    Modifed and extended tests.

--- src/main/java/forum/repositroy/PostingRepository.java
+++ src/main/java/forum/repositroy/PostingRepository.java
@@ -5,13 +5,29 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import forum.document.Posting;
 
 public interface PostingRepository extends MongoRepository<Posting, String> {
 
 	public Optional<Posting> findById(String id);
+
 	public List<Posting> findByMessageBody(String messageBody);
+
 	public List<Posting> findByUserFirstNameAndUserLastName(String userFirstName, String userLastName, Sort sort);
+
+	@Query("{'originalPostingId' : null}")
+	public List<Posting> findOriginalPostingsOnly(Sort sort);
+
+	/**
+	 * 
+	 * Finds comments for an original posting
+	 * @param originalPostingId
+	 * @param sort
+	 * @return
+	 */
+	public List<Posting> findByOriginalPostingId(String originalPostingId, Sort sort);
+
 	public List<Posting> findAll(Sort sort);
 }
